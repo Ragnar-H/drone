18d6aca968138e6e16de36cabc4c9c82ed50fc6f
Ragnar-H <ragnarhardarson90@gmail.com>
2019-02-23 19:35:28 +0100

    Add stream calls

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,9 +2,17 @@ import * as dgram from "dgram";
 import { AddressInfo } from "net";
 
 const PORT = 8889;
+const VIDEO_PORT = 11111;
 const HOST = "192.168.10.1";
 const drone = dgram.createSocket("udp4");
 drone.bind(PORT);
+
+const videoStream = dgram.createSocket("udp4");
+videoStream.bind(VIDEO_PORT);
+
+videoStream.on('message', (msg, rinfo) => {
+  console.log(`stream got: ${msg} from ${rinfo.address}:${rinfo.port}`);
+})
 
 drone.on("error", err => {
   console.log(`server error:\n${err.stack}`);
@@ -29,3 +37,6 @@ function handleError(err: Error | null) {
 
 drone.send("command", 0, 7, PORT, HOST, handleError);
 drone.send("battery?", 0, 8, PORT, HOST, handleError);
+drone.send("streamon", 0, 8, PORT, HOST, handleError);
+
+setTimeout(() => drone.send("streamoff", 0, 9, PORT, HOST, handleError), 5000);
